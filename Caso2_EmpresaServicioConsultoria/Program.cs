399848aa97ebe6c3afcf30f76aa2f3b761570d1f
georgegp8 <george.guerra@tecsup.edu.pe>
2026-04-14 12:00:58 -0500

    intial commit

---
+++ Caso2_EmpresaServicioConsultoria/Program.cs
@@ -0,0 +1,60 @@
+using Caso2_EmpresaServicioConsultoria.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Servicios
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+
+// Configuración de Swagger
+builder.Services.AddSwaggerGen();
+
+
+// Conexión a PostgreSQL (XDbContext)
+builder.Services.AddDbContext<ConsultoriaDB>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Registro de Repositorios (Inyección de Dependencias)
+
+
+
+
+// Registro del Unit of Work
+//builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+// OpenAPI (Estándar de .NET 9)
+builder.Services.AddOpenApi();
+
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+  
+    // Configuración de Swagger para que cargue al inicio
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Universidad API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+
+
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+
+app.Run();
