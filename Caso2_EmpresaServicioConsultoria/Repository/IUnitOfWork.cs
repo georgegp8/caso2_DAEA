506bcc9c9fa1734995181eafc57ce162905e8cfb
Diego Zahid Quispe Quispe <diego.quispe.qui@tecsup.edu.pe>
2026-04-14 15:53:42 -0500

    feat: implementación del patrón Unit of Work con repositorio genérico
    Se implementa IUnitOfWork y UnitOfWork para centralizar el acceso a repositorios,
    permitiendo obtener repositorios genéricos y guardar cambios con SaveChangesAsync.
    Incluye manejo de instancias y liberación de recursos.

--- Caso2_EmpresaServicioConsultoria/Repository/IUnitOfWork.cs
+++ Caso2_EmpresaServicioConsultoria/Repository/IUnitOfWork.cs
@@ -1,6 +1,7 @@
 ﻿namespace Caso2_EmpresaServicioConsultoria.Repository;
 
-public interface IUnitOfWork
+public interface IUnitOfWork : IDisposable
 {
-    
+    IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+    Task<int> Complete();
 }
--- Caso2_EmpresaServicioConsultoria/Repository/Implements/UnitOfWork.cs
+++ Caso2_EmpresaServicioConsultoria/Repository/Implements/UnitOfWork.cs
@@ -1,6 +1,45 @@
-﻿namespace Caso2_EmpresaServicioConsultoria.Repository.Implements;
+﻿using System.Collections;
+using Caso2_EmpresaServicioConsultoria.Models;
 
-public class UnitOfWork
+namespace Caso2_EmpresaServicioConsultoria.Repository.Implements;
+
+public class UnitOfWork : IUnitOfWork
 {
+    private Hashtable? _repository;
+    private readonly ConsultoriaDBContext _dbContext;
+
+    public UnitOfWork(ConsultoriaDBContext context)
+    {
+        _dbContext = context;
+        _repository = new Hashtable();
+    }
+
+    public IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class
+    {
+        var Type = typeof(TEntity).Name;
+
+        if (_repository.ContainsKey(Type))
+        {
+            return (IGenericRepository<TEntity>)_repository[Type];
+        }
+        var repositoryType = typeof(GenericRepository<>);
+        var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(TEntity)), _dbContext);
+        
+        if (repositoryInstance != null)
+        {
+            _repository.Add(Type, repositoryInstance);
+            return (IGenericRepository<TEntity>)repositoryInstance;
+        }
+        throw new Exception($"Couldn't create repository of type {Type}");
+    }
+
+    public Task<int> Complete()
+    {
+        return _dbContext.SaveChangesAsync();
+    }
     
+    public void Dispose()
+    {
+        _dbContext.Dispose();
+    }
 }
